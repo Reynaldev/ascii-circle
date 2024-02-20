2d458d68f1e08352292dd6fb60308f59ecc7d662
Reynaldev <reynaldy.nur.cahyo.003@gmail.com>
2024-02-20 20:02:05 +0700

    Added filled circle type

--- main.cpp
+++ main.cpp
@@ -10,25 +10,58 @@
 
 char grid[GRID_SIZE][GRID_SIZE];
 
-int main(int argc, char **argv)
+void drawCircleOutline(int radius)
 {
-    memset(grid, ' ', sizeof(grid));
-
-    int radius = 5;
-    
-    if (argc == 2)
-        radius = atoi((char*)argv[1]);
-
-    // Make a circle
-    for (int i = 0; i <= 360; i++)
+    for (int i = 0; i < 360; i++)
     {
         float rad = (float) (i * PI / 180);             // Degree to Radian
         int x = round((sin(rad) * radius) + OFFSET);
         int y = round((cos(rad) * radius) + OFFSET);
 
         grid[y][x] = 'O';
-        // printf("Rad: %.2f\t X: %f\t Y: %f\n", rad, x, y);
     }
+}
+
+void drawCircleFill(int radius)
+{
+    for (int i = 0; i <= 180; i++)
+    {
+        float fR = (float) (i * PI / 180);              // Degree to Radian
+        float eR = (float) ((360 - i) * PI / 180);      // Opposite value
+
+        int fx = round((sin(fR) * radius) + OFFSET);
+        int fy = round((cos(fR) * radius) + OFFSET);
+
+        int ex = round((sin(eR) * radius) + OFFSET);
+
+        grid[fy][fx] = 'O';
+
+        for (int j = fx; j > ex; j--)
+        {
+            grid[fy][j] = 'O';
+        }
+    }
+    
+}
+
+int main(int argc, char **argv)
+{
+    memset(grid, ' ', sizeof(grid));
+
+    int radius = (argc == 2) ? atoi((char*)argv[1]) : 5;
+    char type[8];
+
+    memset(type, 0, sizeof(type));
+
+    if (argc == 3)
+        strcpy(type, argv[2]);
+    else
+        strcpy(type, "Outline");
+
+    if (!strcmp(type, "Fill"))
+        drawCircleFill(radius);
+    else if (!strcmp(type, "Outline"))
+        drawCircleOutline(radius);
 
     // Draw
     for (int i = 0; i < GRID_SIZE; i++)
